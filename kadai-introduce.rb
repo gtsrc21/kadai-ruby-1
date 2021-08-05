48f0aa22361b79e2b885b9c3b9c28c7bc1862dd6
taro.kirameki <taro.kirameki@techacademy.jp>
2021-08-05 13:41:13 +0000

    変数の頭文字を小文字に変更

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,5 +1,5 @@
-Family_name = "キラメキ" #苗字を定義
-First_name = "太郎" #名前を定義
-Age = 34 #年齢を定義
+family_name = "キラメキ" #苗字を定義
+first_name = "太郎" #名前を定義
+age = 34 #年齢を定義
 
-puts Family_name + First_name + "です。" + Age.to_s + "歳です。"
+puts family_name + first_name + "です。" + age.to_s + "歳です。"
